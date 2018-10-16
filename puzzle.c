0698974c77216e35f029d84261453fb52758ecb1
Unknown <zirenx@student.unimelb.edu.au>
2018-10-16 18:14:38 +1100

    not going back

--- puzzle.c
+++ puzzle.c
@@ -7,11 +7,10 @@
 #include <sys/types.h>
 #include <sys/resource.h>
 #include <sys/time.h>
-#include <malloc.h>
 
 
 
-/**
+/** 
  * READ THIS DESCRIPTION
  *
  * node data structure: containing state, g, f
@@ -21,6 +20,7 @@ typedef struct node{
 	int state[16];
 	int g;
 	int f;
+    int last_action;
 } node;
 
 /**
@@ -42,7 +42,7 @@ unsigned long expanded;
 
 /**
  * The id of the four available actions for moving the blank (empty slot). e.x.
- * Left: moves the blank to the left, etc.
+ * Left: moves the blank to the left, etc. 
  */
 
 #define LEFT 0
@@ -52,7 +52,7 @@ unsigned long expanded;
 
 /*
  * Helper arrays for the applicable function
- * applicability of operators: 0 = left, 1 = right, 2 = up, 3 = down
+ * applicability of operators: 0 = left, 1 = right, 2 = up, 3 = down 
  */
 int ap_opLeft[]  = { 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 };
 int ap_opRight[]  = { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0 };
@@ -65,11 +65,12 @@ int *ap_ops[] = { ap_opLeft, ap_opRight, ap_opUp, ap_opDown };
 void print_state( int* s )
 {
 	int i;
-
+	
 	for( i = 0; i < 16; i++ )
 		printf( "%2d%c", s[i], ((i+1) % 4 == 0 ? '\n' : ' ') );
+    printf ("\n");
 }
-
+      
 void printf_comma (long unsigned int n) {
     if (n < 0) {
         printf ("-");
@@ -123,7 +124,7 @@ void apply( node* n, int op )
 	//apply op
 	n->state[blank_pos] = n->state[t];
 	n->state[t] = 0;
-
+	
 	//update blank pos
 	blank_pos = t;
 }
@@ -139,12 +140,18 @@ node* ida( node* n, int threshold, int* newThreshold )
 	 */
 	int action = 0;
 	for(action = 0; action < 4; action++){
+        if ((action == 0 && n->last_action == 1) ||
+            (action == 1 && n->last_action == 0) ||     
+            (action == 2 && n->last_action == 3) ||
+            (action == 3 && n->last_action == 2)) {
+            continue;
+        }
 		struct node n_node;
-		generated++;
 		for(int i = 0; i < 16; i++) {
 			if(n->state[i] == 0) blank_pos = i;
 		}
 		if(applicable(action)){
+			generated++;
 			for(int i = 0; i < 16; i++) {
 				n_node.state[i] = n->state[i];
 			}
@@ -152,6 +159,7 @@ node* ida( node* n, int threshold, int* newThreshold )
 			// print_state(n_node.state);
 			n_node.g = n->g + 1;
 			n_node.f = n_node.g + manhattan(n_node.state);
+            n_node.last_action = action;
 			if(n_node.f > threshold){
 				*newThreshold = n_node.f < *newThreshold ? n_node.f : *newThreshold;
 				// printf("%d ", *newThreshold);
@@ -181,9 +189,9 @@ node* ida( node* n, int threshold, int* newThreshold )
 /* main IDA control loop */
 int IDA_control_loop(  ){
 	node* r = NULL;
-
+	
 	int threshold;
-
+	
 	/* initialize statistics */
 	generated = 0;
 	expanded = 0;
@@ -193,7 +201,7 @@ int IDA_control_loop(  ){
 	threshold = initial_node.f;
 
 	printf( "Initial Estimate = %d\nThreshold = ", threshold );
-
+	
 
 	/**
 	 * FILL WITH YOUR CODE
@@ -206,6 +214,7 @@ int IDA_control_loop(  ){
 			generated++;
 			for(int i = 0; i < 16; i++) n.state[i] = initial_node.state[i];
 			n.g = 0;
+            n.last_action = -1;
 			r = ida(&n, threshold, &newB);
 			printf("%d ", threshold);
 			if(r == NULL){
@@ -223,8 +232,8 @@ int IDA_control_loop(  ){
 static inline float compute_current_time()
 {
 	struct rusage r_usage;
-
-	getrusage( RUSAGE_SELF, &r_usage );
+	
+	getrusage( RUSAGE_SELF, &r_usage );	
 	float diff_time = (float) r_usage.ru_utime.tv_sec;
 	diff_time += (float) r_usage.ru_stime.tv_sec;
 	diff_time += (float) r_usage.ru_utime.tv_usec / (float)1000000;
@@ -255,14 +264,14 @@ int main( int argc, char **argv )
 				initial_node.state[i] = atoi( tile );
 				blank_pos = (initial_node.state[i] == 0 ? i : blank_pos);
 				tile = strtok( NULL, " " );
-			}
+			}		
 	}
 	else{
 		fprintf( stderr, "Filename empty\"\n" );
 		return( -2 );
 
 	}
-
+       
 	if( i != 16 )
 	{
 		fprintf( stderr, "invalid initial state\n" );
@@ -278,17 +287,17 @@ int main( int argc, char **argv )
 
 	/* solve */
 	float t0 = compute_current_time();
-
-	solution_length = IDA_control_loop();
+	
+	solution_length = IDA_control_loop();				
 
 	float tf = compute_current_time();
 
 	/* report results */
 	printf( "\nSolution = %d\n", solution_length);
 	printf( "Generated = ");
-	printf_comma(generated);
+	printf_comma(generated);		
 	printf("\nExpanded = ");
-	printf_comma(expanded);
+	printf_comma(expanded);		
 	printf( "\nTime (seconds) = %.2f\nExpanded/Second = ", tf-t0 );
 	printf_comma((unsigned long int) expanded/(tf+0.00000001-t0));
 	printf("\n\n");
@@ -300,6 +309,8 @@ int main( int argc, char **argv )
 	fprintf( report, "\n\tSoulution = %d, Generated = %lu, Expanded = %lu", solution_length, generated, expanded);
 	fprintf( report, ", Time = %f, Expanded/Second = %f\n\n", tf-t0, (float)expanded/(tf-t0));
 	fclose(report);
-
+	
 	return( 0 );
 }
+
+
